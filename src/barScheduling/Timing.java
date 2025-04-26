6e2d7b67f594a8fe7da0317a513e93bdfbe1c7a9
Goamhobala <yehbebe0112@gmail.com>
2025-04-26 10:23:09 +0200

    Created Timing class to store timing data

---
+++ src/barScheduling/Timing.java
@@ -0,0 +1,19 @@
+package barScheduling;
+
+/*
+A struct datatype to store timing information
+*/
+public class Timing {
+    // Ready
+    protected long arrivalTime;
+    // Start 
+    protected long startTime;
+    // First execution
+    protected long firstResponseTime;
+    // Finished task
+    protected long finishTime;
+
+    public void writeOut(String output){
+
+    }
+}
